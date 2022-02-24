1f9efed68f7be9e663651ec7862d6c97d7cf1ac7
Pikrikun <purwafikrialghifari@gmail.com>
2022-02-23 16:16:05 -0800

    pertlite

--- js/javascript.js
+++ js/javascript.js
@@ -4,12 +4,20 @@ M.Slider.init(slider, {
 });
 
 const sidenav = document.querySelectorAll(".sidenav");
-M.Sidenav.init(sidenav, {
-  close: true,
-});
+M.Sidenav.init(sidenav);
 
 const parallax = document.querySelectorAll(".parallax");
 M.Parallax.init(parallax);
 
 const materialbox = document.querySelectorAll(".materialboxed");
 M.Materialbox.init(materialbox);
+
+var toastTrigger = document.getElementById("liveToastBtn");
+var toastLiveExample = document.getElementById("liveToast");
+if (toastTrigger) {
+  toastTrigger.addEventListener("click", function () {
+    var toast = new bootstrap.Toast(toastLiveExample);
+
+    toast.show();
+  });
+}
